3250aee01103c1c90d7351dcf1dcc468c8407ba3
Erwin C <erwin.sistemas@gmail.com>
2021-07-19 20:18:38 -0500

    Proyectilimpio

---
+++ src/main/java/com/devs4j/Rest/MaverAlbo1Application.java
@@ -0,0 +1,13 @@
+package com.devs4j.Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverAlbo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaverAlbo1Application.class, args);
+	}
+
+}
